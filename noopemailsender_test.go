6d7a3f3e8849f844942f489f3ca670b3b38c6211
David Malone <dmalone@pivotal.io>
2016-02-17 12:58:45 -0500

    removed unecessary data struct since this test has been changed to use the simpleMessageBody impl"

--- noopemailsender_test.go
+++ noopemailsender_test.go
@@ -14,17 +14,6 @@ func TestNoopSend(t *testing.T) {
 	NewSender = NewNoopSender
 	sender := NewSender()
 
-	data := struct {
-		Title string
-		Items []string
-	}{
-		Title: "Test page",
-		Items: []string{
-			"First Item",
-			"Second Item",
-		},
-	}
-
 	body := NewSimpleMessageBody("Test simple message body")
 	err := sender.Send(NewMessage("from@test.com", "to@test.com", "Subject", body))
 
